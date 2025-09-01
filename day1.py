4f48917ff8acce8d23965602074ae00ce288e357
Jamshiduzbn <jamshiduzbn@gmail.com>
2025-09-01 10:57:33 +0300

    Add day1.py

---
+++ day1.py
@@ -0,0 +1,11 @@
+print("Hello, Jami!")
+print("Today is the first day of my Python journey!")
+name = "Jamshid"
+age = 52
+print("My name is", name)
+print("I am" , age, "years old")
+a = 7
+b = 5
+print("Sum of a and b is:", a + b)
+city = "Tashkent"
+print(f"My name is {name}, I am {age} years old, and I live in {city}.")
