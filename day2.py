da6024713a0a4ce6c7366517042505e1b3227265
Jamshiduzbn <jamshiduzbn@gmail.com>
2025-09-01 16:22:58 +0300

    Add files via upload

--- day2.py
+++ day2.py
@@ -3,3 +3,13 @@ if age >=18:
     print("You are an adult.")
 else:
     print("You are not an adult.")
+    a = 10
+    b = 3
+    print("a + b =", a + b) 
+    print("a - b =", a - b)
+    print("a * b =", a * b)
+    print("a / b =", a / b)
+    print("a // b =", a // b)
+    print("a % b =", a % b)
+    print("a ** b =", a ** b)
+    
