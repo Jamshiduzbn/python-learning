f5d8630f1802770ecf41f40ba41a695b22b5914b
Jamshiduzbn <jamshiduzbn@gmail.com>
2025-09-06 19:36:19 +0300

    Add day 5
    loops (for, while, break, continue) with exercises

---
+++ day5.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# چاپ اعداد از 1 تا 10
+#for number in range(1, 11):
+#   print("number:", number)
+# از 0 تا 20 با گام 2
+#for i in range(0, 21, 2):
+#    print(i)
+#for i in range(10, 0, -1):
+#    print(i)
+
+#print("Boom!")
+#fruits = ["apple", "banana", "cherry"]
+#for fruit in fruits:
+#    print("I like", fruit)
+#i = 1
+#while i<= 5:
+#    print("step", i)
+#    i += 1
+#for i in range(1, 10):
+#    if i == 5:
+#        break
+#    print(i)
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
