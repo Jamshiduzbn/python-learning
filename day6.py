ae9ab31ae1e39689a3fd93653cb200a5dad6d238
Jamshiduzbn <jamshiduzbn@gmail.com>
2025-09-07 10:41:09 +0300

    Add Day 6
    string operations (index, slice, methods, f-strings)

---
+++ day6.py
@@ -0,0 +1,38 @@
+name = "Jamshid"
+msg = "Hello, Jami!"
+#print(name)
+#print(type(name))
+#print(len(msg))
+word = "Python"
+#print(word[0])
+#print(word[1])
+#print(word[-1])
+#print(word[-2])
+word = "Programing"
+#print(word[0:6])
+#print(word[3:7])
+#print(word[:4])
+#print(word[4:])
+#print(word[-3])
+#print(word[-3:])
+text = "  Hello, Jami!  "
+#print(text.upper())
+#print(text.lower())
+#print(text.strip())
+#print(text.replace("Jami", "Friend"))
+#print(text.split(","))
+first = "Jamshid"
+last = "Uzbn"
+full = first + " " + last
+#print(full)
+name = "Jami"
+age = 52
+msg = f"My name is {name} and I am {age} years old."
+#print(msg)
+text = "I like Python"
+#print(text.replace("Python", "Java"))
+word = "Programing"
+#print(word[:6] + "mer")
+text = "  hello world  "
+print(text.strip().upper())
+
